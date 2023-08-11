635184843228a0b3a2ee3e7a99ba00bbd922aa3e
Sirio <sirioroberto1@hotmail.com>
2023-08-10 22:55:41 -0300

    Implemented JCommander framework to get command line arguments and pass to my server with sockets

--- client/Main.java
+++ client/Main.java
@@ -1,26 +1,45 @@
 package client;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Main {
 
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 34522;
 
+    @Parameter(names = "-t", description = "Type of request (set, get or delete)")
+    private String typeOfRequest;
+
+    @Parameter(names = "-i", description = "Index of the cell array we have on the server (from 1 to 1000)")
+    private int cellIndex;
+
+    @Parameter(names = "-m", description = "Value set on the cell if user uses the 'set' command")
+    private String value;
+
     public static void main(String[] args) {
+        Main main = new Main();
+        JCommander.newBuilder()
+                        .addObject(main)
+                        .build()
+                        .parse(args);
+        main.run();
+    }
+
+    private void run() {
         System.out.println("Client started!");
         try (
                 Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
         ) {
-            String msg = "Give me a record # 13";
-            output.writeUTF(msg);
-            System.out.println("Sent: " + msg);
+            output.writeUTF(getSimplifiedCommand());
+            System.out.println("Sent: " + getSimplifiedCommand());
 
             String receivedMsg = input.readUTF();
             System.out.println("Received: " + receivedMsg);
@@ -28,5 +47,9 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getSimplifiedCommand() {
+        return String.format("%s %s %s!", typeOfRequest, cellIndex, "set".equals(typeOfRequest) ? value : "");
     }
 }
--- server/Main.java
+++ server/Main.java
@@ -1,14 +1,11 @@
 package server;
 
-import javax.imageio.IIOException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     private static final String ADDRESS = "127.0.0.1";
@@ -20,20 +17,13 @@ public class Main {
 
         try (ServerSocket server = new ServerSocket(PORT, 50, InetAddress.getByName(ADDRESS))) {
             System.out.println("Server started!");
-            try (Socket socket = server.accept();
-                 DataInputStream input = new DataInputStream(socket.getInputStream());
-                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
-                String msg = input.readUTF();
-                System.out.println("Received: " + msg);
-
-                Matcher matcher = Pattern.compile("\\d+").matcher(msg);
-                int receivedNum = -1;
-                if (matcher.find()) {
-                    receivedNum = Integer.parseInt(matcher.group());
-                }
-                String msgToSent = String.format("A record # %s was sent!", receivedNum);
-                output.writeUTF(msgToSent);
-                System.out.println("Sent: " + msgToSent);
+            try (
+                    Socket socket = server.accept();
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+            ) {
+                String receivedCommand = input.readUTF();
+                output.writeUTF("OK");
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
